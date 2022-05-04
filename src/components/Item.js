390b95d66a3a1d0532687fc4744cd4cd490ecfaf
Lester <lesterong776@gmail.com>
2022-05-04 15:30:13 +0800

    Enable links in json

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Arrow from "../assets/Arrow";
+import parse from 'html-react-parser';
 
 const Item = ({faq}) => {
   const [isOpen, setIsOpen] = useState(false);
   const question = faq.question;
-  const answer = faq.answer;
+  const answer = parse(faq.answer);
 
   return (
-    <div className="item" onClick={() => setIsOpen(!isOpen)}>
-      <header className="flex items-center place-content-between"> 
+    <div className="item">
+      <header className="flex items-center place-content-between cursor-pointer" onClick={() => setIsOpen(!isOpen)}> 
         <h2> {question} </h2>
         <Arrow isOpen={isOpen} />
       </header>
